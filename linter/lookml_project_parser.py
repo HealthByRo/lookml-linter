003e4fad43b8bd35279dea4236ab3676e7766d79
Rudo Terry <rudo.terry@ro.co>
2022-09-23 12:26:03 -0700

    updating formatting and unused libs

--- linter/lookml_project_parser.py
+++ linter/lookml_project_parser.py
@@ -1,25 +1,25 @@
 import os
 import lkml
-import glob
-import argparse
 
 
 class LookMlProjectParser:
-
-
     def __init__(self, lkml_dir: str) -> None:
-        '''parse a list of LookML filepaths and then read into dictionary object
+        """parse a list of LookML filepaths and then read into dictionary object
         Args:
             infilepath (list): list of input LookML file paths
         Returns:
 
-        '''
+        """
         self.lkml_dir = lkml_dir
         if os.path.isdir(self.lkml_dir):
-            lkml_filepaths = [os.path.join(dp, f) for dp, dn, filenames in os.walk(self.lkml_dir) for f in filenames if os.path.splitext(f)[1] == '.lkml'] #use instead of glob - glob not searching mounted docker volume subdirectories 
+            lkml_filepaths = [
+                os.path.join(dp, f)
+                for dp, dn, filenames in os.walk(self.lkml_dir)
+                for f in filenames
+                if os.path.splitext(f)[1] == ".lkml"
+            ]  # use instead of glob - glob not searching mounted docker volume subdirectories
         else:
-            raise IOError("Directory does not exist: %s" %
-                          self.lkml_dir)
+            raise IOError("Directory does not exist: %s" % self.lkml_dir)
         self.unparsable_lookml_files = []
         self.parsed_lookml_files = {}
 
@@ -28,7 +28,7 @@ class LookMlProjectParser:
             if not os.path.exists(filepath):
                 raise IOError("Filename does not exist: %s" % filepath)
 
-            with open(filepath, 'r') as file:
+            with open(filepath, "r") as file:
                 try:
                     self.parsed_lookml_files[filepath] = lkml.load(file)
                 except SyntaxError:
